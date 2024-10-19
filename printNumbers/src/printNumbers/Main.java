9822b4536632efccf21dba52fc1da6b8b8a778d3
dacq <dacq@dacqs-Laptop.local>
2024-10-19 19:35:46 +0200

    Testing variables and others stuff

---
+++ printNumbers/src/printNumbers/Main.java
@@ -0,0 +1,12 @@
+package printNumbers;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(3);
+		System.out.println(358);
+		System.out.println(50000 + "\n");
+		// math expressions
+		System.out.println(3 + 3);
+		System.out.println(2 * 5);
+	}
+}
